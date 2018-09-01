3d7b465940dbde2ca810f906b2da13f866af2b08
Aaditya Menon <aaditya@sparker0i.me>
2018-09-01 17:48:50 +0530

    Added Phone GSON Model

---
+++ app/src/main/java/com/example/sparker0i/samplenodeapp/Phone.java
@@ -0,0 +1,40 @@
+package com.example.sparker0i.samplenodeapp;
+
+public class Phone {
+    private String model;
+    private String manufacturer;
+    private int price;
+    private int quantity;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ app/src/main/java/com/example/sparker0i/samplenodeapp/PhoneList.java
@@ -0,0 +1,16 @@
+package com.example.sparker0i.samplenodeapp;
+
+import java.util.List;
+
+public class PhoneList {
+    private List<Phone> phones;
+
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+}
